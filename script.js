7d65d523c724f80bf93d46d36c52e653ee2a889d
Athaxv <161715841+Athaxv@users.noreply.github.com>
2024-05-20 19:21:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ Array.from(buttons).forEach((button) => {
       document.querySelector('input').value = string;
     }
     else if (e.target.innerHTML == '()') {
-      string = ((string));
+      string = ("string");
       document.querySelector('input').value = string;
     }
     else {
